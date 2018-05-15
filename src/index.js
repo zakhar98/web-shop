0174585f14003e32fd670bbb278e05955a67fe2c
zakhar98 <zakhar98@yandex.ru>
2018-05-15 13:47:10 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ProductComponent from './components/Product'
 
-ReactDOM.render(<ProductComponent />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
